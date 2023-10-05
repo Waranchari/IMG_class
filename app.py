55dc82c5aa1246fcd22561853192288d5a0b183f
Chaisrithip <Waranchari@users.noreply.huggingface.co>
2023-10-05 15:33:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 import streamlit as st
 from transformers import pipeline
+from PIL import Image
 
-pipe = pipeline("question-answering", model="deepset/roberta-base-squad2")
+pipeline = pipeline(task="image-classification", model="microsoft/resnet-50")
+
+def predict(image):
+    predictions = pipeline(image)
+    return {p["label"]: p["score"] for p in predictions}
+
 def main():
-    st.title("Yelp review")
+    st.title("Image Classification")
+    uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 
-    with st.form("text_field"):
-        text = st.text_area('enter some text:')
-        # clicked==True only when the button is clicked
-        clicked = st.form_submit_button("Submit text")
+    if uploaded_file is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+        clicked = st.form_submit_button("Predict")
         if clicked:
-          results = pipe([text])
-          st.json(results)
+            results = predict(image)
+            sorted_data = sorted(results, reverse=True)
+            top_five = sorted_data[:5]
+            st.success('The predicted image is {}'.format(top_five))
 
 if __name__ == "__main__":
     main()
